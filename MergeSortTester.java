7b5a5e63182b50b69665a19b61de15c35e43d9f3
eVookelj <evukelj@stuy.edu>
2016-03-09 18:30:09 -0500

    Fill in comments for MergeSortTester

---
+++ MergeSortTester.java
@@ -0,0 +1,40 @@
+/*
+Team Phoenix Wrong: Emma, Adam, Owen
+APCS2 pd5
+HW11
+2016-03-09
+ */
+/*======================================
+  class MergeSortTester
+  ALGORITHM:
+  1. If input length==1, return because its sorted.
+  2. Make two new arrays, each taking half of the values from the original.
+  3. Sort these two arrays with recursive calls, and merge them.
+
+  BIG-OH CLASSIFICATION OF ALGORITHM: O(nlogn)
+  Mean execution times for dataset of size n:
+  Batch size: <# of times each dataset size was run>
+  n=1       time: 
+  n=10      time: 
+  n=100     time: 
+  ...
+  n=<huge>  time: 
+  ANALYSIS:
+  <INSERT YOUR RESULTS ANALYSIS HERE>
+  ======================================*/
+
+public class MergeSortTester 
+{
+
+    /******************************
+     * execution time analysis 
+     * <INSERT YOUR DESCRIPTION HERE OF 
+     *  YOUR APPARATUS FOR GENERATING EXECUTION 
+     *  TIME DATA...>
+     ******************************/
+    public static void main( String[] args ) 
+    {
+
+    }//end main
+
+}//end class
