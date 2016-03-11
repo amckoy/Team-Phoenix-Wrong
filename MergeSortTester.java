b42d66184bcfb73d388878bcd2a42ee87be64b13
eVookelj <evukelj@stuy.edu>
2016-03-10 21:01:50 -0500

    Analyze results from testing apparatus

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,9 +1,11 @@
-/*
+
+      /*
 Team Phoenix Wrong: Emma, Adam, Owen
 APCS2 pd5
 HW11
 2016-03-09
  */
+ import java.util.Random;
 /*======================================
   class MergeSortTester
   ALGORITHM:
@@ -13,41 +15,50 @@ HW11
 
   BIG-OH CLASSIFICATION OF ALGORITHM: O(nlogn)
   Mean execution times for dataset of size n:
-  Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
-  ...
-  n=<huge>  time: 
-  ANALYSIS:
-  <INSERT YOUR RESULTS ANALYSIS HERE>
+  Batch size: 10
+  
+  n=10^0  time: ~0.0 ms 
+  n=10^1  time: ~0.0 ms 
+  n=10^2  time: ~0.0 ms
+  n=10^3  time: ~0.0 ms
+  n=10^4  time: ~0.0 ms
+  n=10^5  time: ~16.0 ms
+  n=10^6  time: ~215.0 ms
+  n=10^7  time: ~2721 ms
+  n=10^8  time: <Java heap space error>
+  
+  ANALYSIS: The results from our testing apparatus show that as the dataset gest much bigger, so does the runtime,
   ======================================*/
 
-public class MergeSortTester 
-{
+public class MergeSortTester {
 
     /******************************
      * execution time analysis 
-     * <INSERT YOUR DESCRIPTION HERE OF 
-     *  YOUR APPARATUS FOR GENERATING EXECUTION 
-     *  TIME DATA...>
+     * For 10^0 to 10^10:
+     * Run MergeSort 10 times, and average the times it takes for each one
+     * Print results.
      ******************************/
+     public static int[] genArray(int size) {
+       int[] retArr = new int[size];
+       Random rand = new Random();
+       for (; size>0; size--) { retArr[size-1] = rand.nextInt(); }
+       return retArr;
+     }
+    
     public static void main( String[] args ) 
     {
-      int average = 0;
-      int repeats = 0;
-      while (repeats < 10){
-        int begin = System.currentTimeMillis();
-        //insert algorithm here.......
-        //not sure how to do this....do we just import all of MergeSort and call it here?
+      double average = 0;
+      for (int exponent = 0; exponent < 8; exponent++){
+        for (int repeat = 0; repeat < 10; repeat++){
         
-        int end = System.currentTimeMillis();
-        average += end - begin;
+            int[] arr = genArray((int)(Math.pow(10,exponent)));
+            double begin = System.currentTimeMillis();
+            MergeSort.sort(arr);
+            double end = System.currentTimeMillis();
+          average = end - begin;
+        }
+        System.out.println("Average time for n=10^" + exponent + ": " + average);
+        average = 0;// we good?
       }
-      repeats = 0
-      System.out.println(average / 10);
-      average = 0;
-  
     }//end main
-
 }//end class
